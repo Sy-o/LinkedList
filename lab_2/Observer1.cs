f85e63c463e4523a254e8979ac64822f9e559827
Alice <fieryrobin@gmail.com>
2014-03-10 23:00:39 +0300

    add new observer classes

---
+++ lab_2/Observer1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    public class Observer1 : IObserver
+    {
+        Label stateString;
+
+        public Observer1(ref Label stateString)
+        {
+            this.stateString = stateString; 
+        }
+
+        public void Update(string state)
+        {
+            stateString.Text = "Observer #1: " + state;
+        }
+    }
+}
---
+++ lab_2/Observer2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    class Observer2: IObserver
+    {
+        LinkedList list;
+        TextBox textBox;
+
+        public Observer2(ref TextBox textBox, ref LinkedList list)
+        {
+            this.textBox = textBox;
+            this.list = list;
+        }
+
+        public void Update(string state)
+        {
+            View(state);
+        }
+
+        private void View(string state)
+        {
+            string[] itemList = list.Map();
+            string text = "";
+            foreach (string s in itemList)
+                text += s + "\r\n";
+            textBox.Text = "Observer #2: \r\n" + state + "\r\n------------------\r\n";
+            textBox.Text += text;
+        }
+    }
+}
